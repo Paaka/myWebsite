65140dd1873a056d737c4ec600671c1b34049d3b
Paaka <Mateuszhartabus@gmail.com>
2019-11-08 16:37:37 +0100

    Added styling, and Navbar component

---
+++ src/compontents/NavbarWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavbarWrapper.css'
+
+const NavbarWrapper = () => (
+    <div className="navbar">
+        <p className="navbar__paragraph">Matthew Hartabus / Front-End Developer</p>
+    </div>
+    
+)
+
+export default NavbarWrapper;
