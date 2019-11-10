e18cc5424ca31eff9b3d81ba3b14c033a3030195
Paaka <Mateuszhartabus@gmail.com>
2019-11-10 21:51:47 +0100

    Debugged click with your cursor

--- src/compontents/NavbarWrapper.js
+++ src/compontents/NavbarWrapper.js
@@ -4,6 +4,8 @@ import './NavbarWrapper.css'
 const NavbarWrapper = () => (
     <div className="navbar">
         <p className="navbar__paragraph">Matthew Hartabus / Front-End Developer</p>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> 
+        <button>lol</button>
     </div>
     
 )
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import './main.css'
 
 
+
 ReactDOM.render(<App/> ,document.querySelector('#root'));
 
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", 'top: '+(e.pageY- 10)+"px; left: "+(e.pageX-10)+"px;");
+    cursor.setAttribute("style", 'top: '+(e.pageY- 5)+"px; left: "+(e.pageX-5)+"px;");
 })
