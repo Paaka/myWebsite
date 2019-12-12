6f2ac85b0de202b6df61f352e29d186758ea51dd
Paaka <Mateuszhartabus@gmail.com>
2019-12-12 19:57:29 +0100

    Added AboutMe section

--- src/compontents/AboutMe/AboutMe.js
+++ src/compontents/AboutMe/AboutMe.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import styles from './AboutMe.module.scss'
+import picture from '../../assets/images/temporaryImg.jpg';
 
 const AboutMe = () =>(
-    <div>
-        <a href="https://goo.gl/maps/Wsg9Vk5ivDoHAmxU9">Raciborsko</a>
+    <div className={styles.wrapper}>
+        <img className={styles.wrapper__img} alt="Profile" src={picture}/>
+        <h2 className={styles.wrapper__header}>Hello, There</h2>
     </div>
 )
 
