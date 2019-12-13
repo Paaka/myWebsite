e9692c6781c037cb33018da345277e5a6e30b463
Paaka <Mateuszhartabus@gmail.com>
2019-12-13 15:29:19 +0100

    Added text to About me section

--- src/compontents/AboutMe/AboutMe.js
+++ src/compontents/AboutMe/AboutMe.js
@@ -6,6 +6,16 @@ const AboutMe = () =>(
     <div className={styles.wrapper}>
         <img className={styles.wrapper__img} alt="Profile" src={picture}/>
         <h2 className={styles.wrapper__header}>Hello, There</h2>
+        <hr className={styles.wrapper__hr}/>
+        <p>I'm 21 years old an aspiring front-end developer. Mostly I use React, and pure Javascript</p>
+        <p>I'm come from 
+            <a  className={styles.wrapper__link}
+                href="https://goo.gl/maps/5bz2pXmZiytqssrXA"
+                >Raciborsko, PL</a>
+        </p>
+
+        <hr className={styles.wrapper__hr}/>
+    
     </div>
 )
 
