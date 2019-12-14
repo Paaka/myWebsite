54e7211e95831746f5398f57a45e860169c87123
Paaka <Mateuszhartabus@gmail.com>
2019-12-14 19:00:12 +0100

    Added hobbies items

--- src/compontents/Hobbies/Hobbies.js
+++ src/compontents/Hobbies/Hobbies.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Styles from './Hobbies.module.scss'
+import Book from "../../assets/SVG/book.svg"
 
 const Hobbies = () =>(
     <div className={Styles.Hobbies}>
         <h2 className={Styles.Hobbies__heading}>MY HOBBIES :</h2>
-        
+        <div className={Styles.ItemWrapper}>
+            <img className={Styles.ItemWrapper__img} alt="book" src={Book}></img>
+        </div>
     </div>
 )
 
